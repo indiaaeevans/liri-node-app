f38221a3e0358755732bac105d3a78dad3f3a82d
India Evans <indiaaeevans@gmail.com>
2017-04-27 09:35:02 -0400

    first upload

---
+++ keys.js
@@ -0,0 +1,8 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'DKDDBLwc50iZyhz63QiM2mQU2',
+  consumer_secret: 'X5y1saiMQ6FaE8WlzCI0DWyAypfcXctuIPa6rb4Ej3OmtAuEcF',
+  access_token_key: '857371291376070659-ZkeJCjMAaHuAvt1BlZkEsqbkVmabBZh',
+  access_token_secret: 'NKfdPZkW9rOb14vhqPy3IV2CKty3t0pm8rxdxezAoYMIp',
+}
---
+++ liri.js
@@ -0,0 +1,142 @@
+// grab the data from keys.js. Then store the keys in a variable.
+var keys = require("./keys.js");
+// load the twitter, spotify, and request packages --- allows you to access all the functions and properties from the variable name
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+var filesystem = require('fs');
+
+// save the user input to a variable
+let userRequest = process.argv[2],
+	userQuery = "";
+
+// Capture all the words in the user input (ignoring the first three Node arguments)
+for (var i = 3; i < process.argv.length; i++) {
+
+  // Build a string with the user input.
+  userQuery = userQuery + " " + process.argv[i];
+
+}
+
+if (userRequest === "my-tweets"){
+	tweets();
+} else if (userRequest === "spotify-this-song"){
+	if (userQuery){
+		spotifySong(userQuery);
+	} else {
+	// If the user doesn't type a song in, output data for the song "The Sign" by Ace of Base
+		spotifySong("The Sign");
+	}
+} else if (userRequest === "movie-this") {
+	if (userQuery){
+		movie(userQuery);
+	} else {
+	// If the user doesn't type a movie in, output data for the movie 'Mr. Nobody.'
+		movie("Mr. Nobody");
+	}
+} else if (userRequest === "do-what-it-says") {
+	doSomething();
+} else {
+	console.log("That is not a valid request.");
+}
+
+// show your last 20 tweets and when they were created
+function tweets(){
+	// Twitter developer credentials in the form of a set of consumer and access tokens/keys.
+	var client = new Twitter({
+	  consumer_key: keys.twitterKeys.consumer_key,
+	  consumer_secret: keys.twitterKeys.consumer_secret,
+	  access_token_key: keys.twitterKeys.access_token_key,
+	  access_token_secret: keys.twitterKeys.access_token_secret
+	});
+	// sets the variable params to search the username moon_pie_castle
+	// only return back the last 20 tweets
+	// doesn't trim the username so the username information will come up instead of the twitter id#
+	var params = {
+		screen_name: 'moon_pie_castle', 
+		count: 10,
+		trim_user: false
+	};
+	// call to twitter to retrieve the user timeline
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	  	// for each tweet in the response object
+	  	for(tweet in tweets){
+	  		// creates a variable to store the date of each tweet
+	  		var dateCreated = new Date(tweets[tweet].created_at);
+	  		// display the tweet #, date created, and text
+	  		console.log("Tweet " + (parseInt(tweet)+1) + " ");
+	  		console.log(dateCreated.toString().slice(0, 24) + " ");
+	    	console.log(tweets[tweet].text);
+	    	console.log("\n");
+	    }
+	  }
+	});
+}
+
+function spotifySong(song) {
+	spotify.search({ type: 'track', query: song }, function(err, data) {
+    	if ( err ) {
+        	console.log('Error occurred: ' + err);
+        	return;
+    	}
+    // grab the first track result
+    var firstResult = data.tracks.items[0],
+        // we want the: artist(s), song name, preview link, and album
+    	artist = firstResult.artists[0].name,
+    	name = firstResult.name,
+    	link = firstResult.preview_url,
+    	album = firstResult.album.name;
+    // display the song information
+ 	console.log("Artist: " + artist + "\n" + "Album: " + album + "\n" + "Song: " + name + "\n" + "Preview: " + link);
+	});
+}
+
+function movie(movie) {
+	var options = {  
+	  method: 'GET',
+	  uri: 'http://www.omdbapi.com',
+	  qs: {
+	  	t: movie,
+	  	tomatoes: true,
+		},
+	}
+	// use Request to grab data from the [OMDB API]
+	request(options, function (error, response, body) {
+		if(error){
+	  		console.log('error:', error); // Print the error if one occurred 
+	  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+	  	} else {
+	  		var result = JSON.parse(body),
+	  			title = result.Title,
+	  			year = result.Year,
+	  			rating = result.imdbRating,
+	  			country = result.Country,
+	  			language = result.Language,
+	  			plot = result.Plot,
+	  			cast = result.Actors
+	  			tomatoes = result.tomatoURL;
+	  			// show the movie information
+	  			console.log("Title: " + title);
+	  			console.log("Year: " + year);
+	  			console.log("Rating: " + rating);
+	  			console.log("Country: " + country);
+	  			console.log("Language: " + language);
+	  			console.log("Plot: " + plot);
+	  			console.log("Cast: " + cast);
+	  			console.log("Link: " + tomatoes);
+	  	}
+	});
+}
+// Using the `fs` Node package, LIRI will take the text inside of random.txt 
+// and then use it to call one of LIRI's commands.
+function doSomething() {
+	console.log("test");
+	fs.readFile("random.txt", "utf8", function(error, data) {
+			
+	});
+}
+
+// * In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
+// * Make sure you append each command you run to the `log.txt` file. 
+// * Do not overwrite your file each time you run a command.
